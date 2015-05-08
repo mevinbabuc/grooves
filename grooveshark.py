40bccd91d0bf89c518820e666e3f2cd096866433
Mevin <mevinbabuc@gmail.com>
2015-05-08 16:47:15 +0530

    Can crawl and download songs from multiple pages

--- grooveshark.py
+++ grooveshark.py
@@ -1,31 +1,37 @@
-# author
-# mevinbabuc@gmail.com
-
 from bs4 import BeautifulSoup
 import requests
 from urllib import quote
 
-search_term = "rascal flatts"
-page = requests.get("http://grooveshark.io/search/{0}".format(quote(search_term)))
 
-soup = BeautifulSoup(page.content)
+## Settings ##
+search_term = "50 cent"
 
-unique_song_list = list()
+# No of pages of songs to download
+pages = 1
 
-for anchor in soup.findAll('a', href=True, download=True):
+## End Settings ##
 
-    if anchor['download'] in unique_song_list:
-        continue
-    else:
-        with open(anchor['download'], 'wb') as handle:
-            response = requests.get(anchor['href'], stream=True)
+for page in range(1, pages+1):
+    site = requests.get("http://grooveshark.io/search/{0}/{1}".format(quote(search_term), page))
 
-            if not response.ok:
-                continue
+    soup = BeautifulSoup(site.content)
 
-            for block in response.iter_content(1024):
-                if not block:
-                    break
+    unique_song_list = list()
 
-                handle.write(block)
-        print anchor['download'], "Downloaded"
+    for anchor in soup.findAll('a', href=True, download=True):
+
+        if anchor['download'] in unique_song_list:
+            continue
+        else:
+            with open(anchor['download'], 'wb') as handle:
+                response = requests.get(anchor['href'], stream=True)
+
+                if not response.ok:
+                    continue
+
+                for block in response.iter_content(1024):
+                    if not block:
+                        break
+
+                    handle.write(block)
+            print anchor['download'], "Downloaded"
